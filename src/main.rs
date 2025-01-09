476f7b06626bbf4910ccc31331815c32612a86b1
ArjobanSingh <arjobansingh1998@gmail.com>
2025-01-10 03:07:34 +0530

    solved A: knight tournamet efficiently with Segment tree

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,104 @@
 use std::io::{self, BufWriter, Read, Write};
 
-// TC: O(M * (lN to rN)) // where L and R are ith idx of N
-// l and R can be full range of N in each fight, so this boils down to
-// TC: O(N * M)
+mod trees {
+    #[derive(Debug)]
+    pub struct SegmentTree {
+        n: usize,
+        tree: Vec<i32>,
+        lazy: Vec<i32>,
+    }
+
+    impl SegmentTree {
+        pub fn new(n: usize) -> Self {
+            let size = n * 4;
+            SegmentTree {
+                n,
+                tree: vec![0; size],
+                lazy: vec![0; size],
+            }
+        }
+
+        fn propogate(&mut self, node: usize, l: usize, r: usize) {
+            if self.lazy[node] != 0 {
+                self.tree[node] = self.lazy[node];
+                if l != r {
+                    self.lazy[node * 2] = self.lazy[node];
+                    self.lazy[node * 2 + 1] = self.lazy[node];
+                }
+                self.lazy[node] = 0;
+            }
+        }
+
+        fn update_range_inner(
+            &mut self,
+            node: usize,
+            l: usize,
+            r: usize,
+            ql: usize,
+            qr: usize,
+            value: i32,
+        ) {
+            self.propogate(node, l, r);
+
+            // No overlap
+            if r < ql || l > qr {
+                return;
+            }
+
+            // total overlap
+            if ql <= l && r <= qr {
+                self.lazy[node] = value;
+                self.propogate(node, l, r);
+                return;
+            }
+
+            // partial overlap
+            let mid = l + (r - l) / 2;
+            self.update_range_inner(node * 2, l, mid, ql, qr, value);
+            self.update_range_inner(node * 2 + 1, mid + 1, r, ql, qr, value);
+        }
+
+        pub fn update_range(&mut self, ql: usize, qr: usize, value: i32) {
+            self.update_range_inner(1, 1, self.n, ql, qr, value);
+        }
+
+        fn get_query_inner(
+            &mut self,
+            node: usize,
+            l: usize,
+            r: usize,
+            ql: usize,
+            qr: usize,
+        ) -> Option<i32> {
+            self.propogate(node, l, r);
+
+            // No overlap
+            if r < ql || l > qr {
+                return None;
+            }
+
+            // total overlap
+            if ql == l && r == qr {
+                return Some(self.tree[node]);
+            }
+
+            // partial overlap
+            let mid = l + (r - l) / 2;
+
+            // In our case we need to reach the leaf nodes, so the ans will only
+            // be in 1 of the 2 children trees.
+            if let Some(ans) = self.get_query_inner(node * 2, l, mid, ql, qr) {
+                return Some(ans);
+            }
+            return self.get_query_inner(node * 2 + 1, mid + 1, r, ql, qr);
+        }
+
+        pub fn get_query(&mut self, ql: usize, qr: usize) -> Option<i32> {
+            self.get_query_inner(1, 1, self.n, ql, qr)
+        }
+    }
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin()
@@ -13,7 +109,7 @@ fn main() {
     let mut lines = input.lines();
     let mut out = BufWriter::new(io::stdout().lock());
 
-    let nums: Vec<i32> = lines
+    let nums: Vec<usize> = lines
         .next()
         .expect("Error reading line")
         .split_ascii_whitespace()
@@ -21,33 +117,29 @@ fn main() {
         .collect();
 
     let n = nums[0];
-    let m = nums[1];
-    let mut result = vec![0; (n + 1) as usize];
 
-    for _ in 0..m {
-        let fight: Vec<i32> = lines
-            .next()
-            .expect("Error reading line")
+    let mut st = trees::SegmentTree::new(n);
+    for line in lines.rev() {
+        let nums: Vec<i32> = line
             .split_ascii_whitespace()
             .filter_map(|num| num.parse().ok())
             .collect();
 
-        let l = fight[0];
-        let r = fight[1];
-        let winner = fight[2];
+        let l = nums[0];
+        let r = nums[1];
+        let w = nums[2];
 
-        for cur in l..=r {
-            if cur == winner {
-                continue;
-            }
-            if result[cur as usize] == 0 {
-                result[cur as usize] = winner;
-            }
+        if l < w {
+            st.update_range(l as usize, w as usize - 1, w);
+        }
+
+        if w < r {
+            st.update_range(w as usize + 1, r as usize, w);
         }
     }
 
-    result.iter().skip(1).for_each(|&winner| {
-        write!(out, "{} ", winner).expect("Error writing");
+    (1..=n).for_each(|i| {
+        write!(out, "{:?} ", st.get_query(i, i).unwrap()).expect("Error writing answer");
     });
 
     writeln!(out).expect("Error writing newline");
